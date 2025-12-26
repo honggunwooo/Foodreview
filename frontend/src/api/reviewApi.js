9ddc7436222390cce683902c9a82eb92033c4f94
홍건우 <hongkxh@gmail.com>
2025-12-27 05:36:43 +0900

    수정

--- frontend/src/api/reviewApi.js
+++ frontend/src/api/reviewApi.js
@@ -1,5 +1,5 @@
 const API_BASE = 'http://54.180.96.130:3000/api';
-const UPLOAD_BASE = 'http://localhost:3000/api/uploads';
+const UPLOAD_BASE = 'http://54.180.96.130:3000/api/uploads';
 
 export const fetchReviews = async () => {
     const res = await fetch(API_BASE);
